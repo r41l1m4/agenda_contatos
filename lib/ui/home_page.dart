9073c5d6ec3b7959dc71e0426fea1da2dbdfe4b4
Raí <rai.lima17@live.com>
2019-06-27 21:30:09 -0300

    impl opções ligar e foto do contato

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:agenda_contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -116,7 +117,10 @@ class _HomePageState extends State<HomePage> {
                       Padding(
                         padding: EdgeInsets.all(10.0),
                         child: FlatButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            launch("tel:${contacts[index].phone}");
+                            Navigator.pop(context);
+                          },
                           child: Text("Ligar",
                             style: TextStyle(color: Colors.red, fontSize: 20.0),),
                         ),
